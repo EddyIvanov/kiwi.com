eac665d0ee98f7ada0f1d9ff0b05de17cf991bfe
Eddy <eddy.ivanov@e-share.us>
2020-10-11 19:51:40 +0300

    Install eslint-config-kiwicom

---
+++ .eslint.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@kiwicom/eslint-config',
+
+        // - OR -
+        // '@kiwicom/eslint-config/strict',
+
+        // - OR -
+        // '@kiwicom/eslint-config/nitro',
+    ],
+};
